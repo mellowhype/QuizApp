db7a6da1ef9c490151ad2447d7a4298fd9587ff1
mellowhype77 <howard.marks98@gmail.com>
2020-11-07 15:18:47 +0200

    quiz app

---
+++ app/src/main/java/ru/startandroid/quizapp/QuestionActivity.java
@@ -0,0 +1,32 @@
+package ru.startandroid.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+public class QuestionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question);
+
+        Gson gson = new Gson();
+        String jsonFileString = JSONHelper.getJsonFromAssets(getApplicationContext());
+        Type listUserType = new TypeToken<List<Question>>() {
+        }.getType();
+        List<Question> questions = gson.fromJson(jsonFileString, listUserType);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+        QuestionAdapter adapter = new QuestionAdapter(this, questions);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutFrozen(true);
+    }
+}
